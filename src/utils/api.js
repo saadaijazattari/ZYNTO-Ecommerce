b2d3a3c02a40cb3ea5038487bd6787e3cb9e00be
saadaijazattari <saadaijaz1234@>
2026-03-02 02:01:14 +0500

    maked it ready for deployment

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,12 @@
-// utils/api.js - Axios instance with base URL
+// utils/api.js - Axios instance with base 
+
 
 import axios from 'axios';
 
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: API_URL,
     headers: {
         'Content-Type': 'application/json'
     }
