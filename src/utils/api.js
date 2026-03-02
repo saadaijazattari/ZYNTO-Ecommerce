18cee4bc68480e05b3ae210818ffe28b14294e34
saadaijazattari <saadaijaz1234@>
2026-03-02 06:41:20 +0500

    added .env file

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@
 
 import axios from 'axios';
 
-const API_URL = 'https://zynto-ecommerce-backend.vercel.app/api';
+const API_URL = import.meta.env.VITE_BACKEND_BASE_URL || 'http://localhost:5000/api';
 
 const api = axios.create({
     baseURL: API_URL,
